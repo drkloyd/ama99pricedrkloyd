b4c54bf5190f8e02ad7c30c4d4cc2e510242e95d
drkloyd <112909515+drkloyd@users.noreply.github.com>
2025-07-16 17:18:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("🔍 Fiyatlar çekiliyor, lütfen bekleyiniz...")
     prices_text, image_url, product_title, _ = await get_prices_simple(asin)
-    signature = "\n\n🔥Ens🔥Hsn🔥Ibr🔥Kad🔥Onr🔥Sdk🔥Ilk🔥"
+    signature = ""
     message_text = f"*{product_title}*\n\n{prices_text}{signature}"
 
     if image_url.startswith("http"):
